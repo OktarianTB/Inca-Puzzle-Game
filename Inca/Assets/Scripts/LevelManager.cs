badfbce6a3508357f59700fb14aec12b4ce5ac72
Oktarian <oktotb@gmail.com>
2019-04-03 19:48:58 +0200

    Added main menu UI & LevelManager

---
+++ Inca/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int numberOfScenes = SceneManager.sceneCountInBuildSettings;
+
+        if(currentSceneIndex < numberOfScenes - 1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            LoadMenu();
+        }
+
+
+    }
+
+}
